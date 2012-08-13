df45a37bc10ffce5b6e7e89a7c64a742406862d7
Jiri Pinkava <j-pi@seznam.cz>
2012-08-13 18:38:37 +0200

    přidána nová verze knihovny pro konverz T <-> R pro Pt

---
+++ src/include/lib/thermometer_pt.h
@@ -0,0 +1,13 @@
+#ifndef __THERMOMETER_PT_H__
+#define __THERMOMETER_PT_H__
+
+#include <inttypes.h>
+
+typedef uint16_t FP_2_14_t;
+// Temperature with precision 1/20 °C
+typedef uint16_t temp_1_20_t;
+
+FP_2_14_t Pt_TtoR(temp_1_20_t T);
+temp_1_20_t Pt_RtoT(FP_2_14_t R);
+
+#endif
