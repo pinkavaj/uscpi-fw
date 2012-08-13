c42003bbbefb413110b061db641fe14f2a8dcb6b
Jiri Pinkava <j-pi@seznam.cz>
2012-08-13 18:38:37 +0200

    přidány návrhy na další struktury

--- src/lib/heating.c
+++ src/lib/heating.c
@@ -19,7 +19,21 @@ typedef struct {
         uint8_t spi_dev_num;
         uint8_t Ichannel;
         uint8_t Uchannel;
+        /* FIXME: tohle může přijít i jinam */
+        uint16_t Icode_offs;
+        uint16_t Ucode_offs;
 } daq_param_t;
+
+/* FIXME: */
+typedef struct {
+        uint16_t Imin;
+        uint16_t Imax;
+        uint16_t Umin;
+        uint16_t Umax;
+} daq_limits_t;
+
+/* TODO: stavy vstupu (ok, zkrat, .. ?) */
+/* TODO: uint32_t R_slope */
 
 typedef struct {
         daq_param_t daq;
