10a1de5a65500a03027ea14fb5cc06fc17184c66
Jiri Pinkava <j-pi@seznam.cz>
2012-08-13 18:38:34 +0200

    dummy cyklus je volitelnou součástí měření

--- src/drivers/ad974.c
+++ src/drivers/ad974.c
@@ -43,17 +43,16 @@ static void ad974_set_channel(uint8_t channel)
 	BIT_SET(AD974_PORT, AD974_WR);
 }
 
-static uint16_t ad974_get_sample_(void)
+static uint16_t ad974_get_sample_(uint8_t dummy_clk)
 {
-	uint16_t val;
-
 	BIT_CLR(AD974_PORT, AD974_CS);
 	/* start conversion, 10ns CS->RC */
 	BIT_CLR(AD974_PORT, AD974_RC);
 	/* Dummy clock cycle, enable generation of synchronization pulse.
 	 * This is necesary because sync. pulse is unpredictable spontaneously
 	 * generated when SPI is used to comunicate to other device(s). */
-	SPI_dummy_clk();
+	if (dummy_clk)
+		SPI_dummy_clk();
 	/* Wait until conversion complete 4us + few x0 ns to set-up signals. */
 	_delay_us(4.2);
 //	loop_until_bit_is_set(AD974_PORT, AD974_BUSY);
@@ -62,29 +61,31 @@ static uint16_t ad974_get_sample_(void)
 	BIT_SET(AD974_PORT, AD974_RC);
 	_delay_us(1.2);
 	/* dummy clock, starts synchronization pulse */
-	SPI_dummy_clk();
+	if (dummy_clk)
+		SPI_dummy_clk();
 	_delay_loop_1(1);
-	val = SPI_transfer8b(0) << 8;
-	val |= SPI_transfer8b(0);
+	uint8_t valHi = SPI_transfer8b(0);
+	uint8_t valLo = SPI_transfer8b(0);
 
 	BIT_SET(AD974_PORT, AD974_CS);
 
-	return val;
+	return (valHi<<8) + valLo;
 }
 
 uint16_t ad974_get_sample(uint8_t channel)
 {
 	ad974_set_channel(channel);
-	return ad974_get_sample_();
+	return ad974_get_sample_(1);
 }
 
 void ad974_get_samples(uint8_t channel, uint32_t *val, uint8_t count)
 {
 	ad974_set_channel(channel);
-	/* dummy read in order to drop data from previous channel */
-	ad974_get_sample_();
+	/* Dummy read. First value might be invalid if dummy clock cycle is 
+	 * not used. */
+	ad974_get_sample_(0);
 
 	while(count--)
-		*val += ad974_get_sample_();
+		*val += ad974_get_sample_(0);
 }
 
